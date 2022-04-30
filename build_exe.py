1db87e2c16a1716d2ded9fe58faa0aa29292081b
ktxo <gv.subscriptions@gmail.com>
2022-04-30 10:56:59 -0300

    First version

---
+++ build_exe.py
@@ -0,0 +1,16 @@
+"""
+Build executable file using pyisntaller
+"""
+
+import PyInstaller.__main__
+
+# 16 bytes
+app_encrypt_key="MaZ1!-%!ls98A-2X"
+
+PyInstaller.__main__.run([
+    "ktxo/app/main_template.py",
+    "--onefile",
+    f"--key={app_encrypt_key}",
+    "--clean",
+    "--windowed"
+])
