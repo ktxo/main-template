a27331d0b935363b180c076b854e70f398ee1212
ktxo <gv.subscriptions@gmail.com>
2022-12-15 21:52:30 -0300

    Adding pandoc to generate README.html from README.md

--- build_exe.py
+++ build_exe.py
@@ -4,6 +4,7 @@ Build executable file using pyisntaller
 
 import PyInstaller.__main__
 
+
 # 16 bytes
 app_encrypt_key="MaZ1!-%!ls98A-2X"
 
@@ -13,3 +14,11 @@ PyInstaller.__main__.run([
     f"--key={app_encrypt_key}",
     "--clean"
 ])
+
+# Command: pandoc README.md -s -o README.html
+try:
+    import pandoc
+    md = pandoc.read(file="README.md", format="markdown")
+    pandoc.write(doc=md, file="README.html", options=["-s"])
+except:
+    pass
